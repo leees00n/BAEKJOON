bc3513c05fa20f37b9d307c9b4bcd2c66d10b028
leees00n <leees0053@gmail.com>
2022-06-22 21:24:12 +0900

    BaekJoon 18108

---
+++ src/num18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class num18108 {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        int B = in.nextInt();
+
+        System.out.println(B-543);
+        in.close();
+    }
+}
