034b4182ee179a6cf63bd788c0102b9dd6bb8d58
leees00n <leees0053@gmail.com>
2022-06-24 16:38:35 +0900

    BaekJoon 1001 BufferedReader

---
+++ src/num1001_b.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class num1001_b {
+    public static void main (String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a-b);
+
+
+    }
+}
