3f1ef314fe8607e0fafac71e8205aadbc3110367
leees00n <leees0053@gmail.com>
2022-06-30 15:20:51 +0900

    BaekJoon

---
+++ src/num2741_Buffered.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class num2741_Buffered {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        int i = 1;
+        for(i=1;i<N+1;i++) {
+            System.out.println(i);
+        }
+    }
+}
