0c587640b026db036e25ae53ed293df85027eb45
leees00n <leees0053@gmail.com>
2022-06-30 12:55:52 +0900

    BaekJoon

---
+++ src/num2741.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class num2741 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.close();
+
+        int i = 1;
+
+        for (i = 1; i < N + 1; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
+
