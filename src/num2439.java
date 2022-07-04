38d8510dd659a60d51ac5a1b39da9dd8dae39ad1
leees00n <leees0053@gmail.com>
2022-07-04 13:04:48 +0900

    BaekJoon

--- src/num2439.java
+++ src/num2439.java
@@ -1,22 +1,23 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class num2439 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
+        br.close();
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N-i; j++) {
-                System.out.print(" ");
+            bw.write(' ');
             }
                 for (int t = 1; t <= i; t++) {
-                    System.out.print("*");
+            bw.write('*');
                 }
-                System.out.println();
-            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
         }
     }
