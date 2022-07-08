439b3ff94f53a67e3582222c59d9e7992b1e5586
leees00n <leees0053@gmail.com>
2022-07-08 23:53:52 +0900

    BaekJoon

---
+++ src/num10818.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class num10818 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int [] arr = new int [N];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        for (int i = 0; i < N; i++){
+
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        bw.write(arr[0] + " "+ arr[N-1]);
+        bw.flush();
+    }
+}
