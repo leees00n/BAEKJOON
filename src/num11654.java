c713d5b4824b4d9af62f6cca0b7797f682c9142b
leees00n <leees0053@gmail.com>
2022-07-29 16:37:31 +0900

    BaekJoon

---
+++ src/num11654.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class num11654 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int alp = br.read();
+
+        System.out.println(alp);
+    }
+}
