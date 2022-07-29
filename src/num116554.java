3cdd5d5b5cb0bffa0a104828d637be805a2aae93
leees00n <leees0053@gmail.com>
2022-07-29 16:33:04 +0900

    BaekJoon

---
+++ src/num116554.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class num116554 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int value = in.next().charAt(0);
+        System.out.println(value);
+    }
+}
